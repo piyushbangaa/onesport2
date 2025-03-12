ef97be1862eb25e386d328675599d545ada42593
piyushbangaa <piyushbanga2003@gmail.com>
2025-03-12 19:32:55 +0530

    added footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,19 @@ import { TextHoverEffect } from "./TextHoverEffect";
 
 export default function TextHoverEffectDemo() {
   return (
-    <div className="h-190rem flex items-center justify-center">
-      <TextHoverEffect text="OneSport" />
+    <div className="min-h-[250px] flex flex-col ">
+      <div className="">
+        <TextHoverEffect text="OneSport" />
+      </div>
 
-
-
+      <footer className=" p-4 flex justify-end  text-gray-500">
+        <div className="flex flex-col gap-6 ">
+          <a href="#" className="hover:text-blue-500 transition">Privacy Policy</a>
+          <a href="#" className="hover:text-blue-500 transition">Terms of Service</a>
+          <a href="#" className="hover:text-blue-500 transition">Contact Us</a>
+          <a href="#" className=""> Designed By Piyush Banga</a>
+        </div>
+      </footer>
     </div>
   );
 }
