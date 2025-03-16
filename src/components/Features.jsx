340258c4e4437a43cbfaf2a61bb1c239d3d3b8c3
piyushbangaa <piyushbanga2003@gmail.com>
2025-03-16 13:07:50 +0530

    fix

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
 
-const FeatureCard = ({ imageSrc }) => {
+const FeatureCard = ({ imageSrc, h1 }) => {
   return (
     <Tilt
       className="mt-20 w-[300px] h-[260px] bg-black border-2 rounded-lg shadow-xl ml-7 flex justify-center items-center"
@@ -31,7 +31,7 @@ const Features = () => {
       <div className="flex flex-col md:flex-row gap-12">
         <FeatureCard imageSrc="https://images.unsplash.com/photo-1570498839593-e565b39455fc?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <FeatureCard imageSrc="https://images.unsplash.com/photo-1602357280104-742c517a1d82?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFza2V0YmFsbCUyMGNvdXJ0fGVufDB8fDB8fHww" />
-        <FeatureCard imageSrc="/images/peak-hour.jpg" />
+        <FeatureCard imageSrc="https://images.unsplash.com/photo-1567220720374-a67f33b2a6b9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVubmlzJTIwY291cnR8ZW58MHx8MHx8fDA%3D" />
       </div>
     </div>
   );
