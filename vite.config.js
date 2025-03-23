a0c32eb07cc0a3329e96a1009489f3f976fd2098
piyushbangaa <piyushbanga2003@gmail.com>
2025-03-24 01:46:40 +0530

    fix for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),
-    react()
-  ],
-})
+  plugins: [react()],
+  base: "/", 
+  build: {
+    outDir: "dist",
+  }
+});
